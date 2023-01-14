8c925e16b3de5fca0af8427eed04472591893088
UKON256 <tkhs.right2008@gmail.com>
2023-01-14 11:38:43 +0900

    プロジェクト ファイルを追加します。

---
+++ UltimateRoleCreator/Assets/Utils/AutoUpdater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateRoleCreator.Assets.Utils
+{
+    internal class AutoUpdater
+    {
+    }
+}
